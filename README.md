a5db14958746601db64b2fafa00c9f6fef22977c
Harsh Singh <thakurharsh.9304@gmail.com>
2025-10-08 20:47:13 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OpenCV
+
+read - a pixel carries three values which has input from 0 -255 and three are RGB.
---
+++ read.py
@@ -0,0 +1,4 @@
+import cv2 as cv
+img=cv.imread('photos/gdg.png')#READ IMAGE
+cv.imshow('gdg',img)
+cv.waitKey(0)
